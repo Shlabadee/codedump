cd38f68954405d9f43eb969932acd77f38757aa2
Shlabadee <84961162+Shlabadee@users.noreply.github.com>
2024-12-24 21:23:46 -0500

    Added additional functions to endianness.h.

--- endianness/endianness.c
+++ endianness/endianness.c
@@ -48,7 +48,26 @@ void tobigend32(uint8_t* buffer, uint32_t n)
 	}
 }
 
+void tobigend16(uint8_t* buffer, uint16_t n)
+{
+	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
+	{
+		buffer[k] = (uint8_t);
+		n >>= 8;
+		--k;
+	}
+}
+
 void tolitend32(uint8_t* buffer, uint32_t n)
+{
+	for (unsigned int i = 0; i < sizeof(n); ++i)
+	{
+		buffer[i] = (uint8_t)n;
+		n >>= 8;
+	}
+}
+
+void tolitend16(uint8_t* buffer, uint16_t n)
 {
 	for (unsigned int i = 0; i < sizeof(n); ++i)
 	{
@@ -74,6 +93,29 @@ uint32_t frombigend32(uint8_t* buffer)
 	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
 	{
 		n |= (uint32_t)buffer[i] << (8 * k);
+		--k;
+	}
+
+	return n;
+}
+
+uint16_t fromlitend16(uint8_t* buffer)
+{
+	uint16_t n = 0;
+	for (unsigned int i = 0; i < sizeof(n); ++i)
+	{
+		n |= (uint16_t)buffer[i] << (8 * i);
+	}
+
+	return n;
+}
+
+uint16_t frombigend16(uint8_t* buffer)
+{
+	uint16_t n = 0;
+	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
+	{
+		n |= (uint16_t)buffer[i] << (8 * k);
 		--k;
 	}
 
--- endianness/endianness.h
+++ endianness/endianness.h
@@ -14,9 +14,13 @@ bool chkend();
 void tobigend64(uint8_t* buffer, uint64_t n);
 void tobigend32f(uint8_t* buffer, float f);
 void tobigend32(uint8_t* buffer, uint32_t n);
+void tobigend16(uint8_t* buffer, uint16_t n);
 void tolitend32(uint8_t* buffer, uint32_t n);
+void tolitend16(uint8_t* buffer, uint16_t n);
 uint32_t fromlitend32(uint8_t* buffer);
 uint32_t frombigend32(uint8_t* buffer);
+uint32_t fromlitend16(uint8_t* buffer);
+uint32_t frombigend16(uint8_t* buffer);
 
 #ifdef __cplusplus
 }
