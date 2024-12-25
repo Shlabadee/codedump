c695805962eb5b5dbb8781d366e95d15771fe118
Shlabadee <84961162+Shlabadee@users.noreply.github.com>
2024-12-24 21:56:54 -0500

    Converted library into a hybrid library (works best on little endian systems).

--- endianness/endianness.c
+++ endianness/endianness.c
@@ -3,26 +3,6 @@
 
 #include "endianness.h"
 
-void tobigend64(uint8_t* buffer, uint64_t n)
-{
-	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
-	{
-		buffer[k] = (uint8_t)n;
-		n >>= 8;
-		--k;
-	}
-}
-
-void tobigend32(uint8_t* buffer, uint32_t n)
-{
-	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
-	{
-		buffer[k] = (uint8_t)n;
-		n >>= 8;
-		--k;
-	}
-}
-
 void tobigend16(uint8_t* buffer, uint16_t n)
 {
 	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
@@ -30,15 +10,6 @@ void tobigend16(uint8_t* buffer, uint16_t n)
 		buffer[k] = (uint8_t)n;
 		n >>= 8;
 		--k;
-	}
-}
-
-void tolitend32(uint8_t* buffer, uint32_t n)
-{
-	for (unsigned int i = 0; i < sizeof(n); ++i)
-	{
-		buffer[i] = (uint8_t)n;
-		n >>= 8;
 	}
 }
 
@@ -68,29 +39,6 @@ uint32_t frombigend32(uint8_t* buffer)
 	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
 	{
 		n |= (uint32_t)buffer[i] << (8 * k);
-		--k;
-	}
-
-	return n;
-}
-
-uint16_t fromlitend16(uint8_t* buffer)
-{
-	uint16_t n = 0;
-	for (unsigned int i = 0; i < sizeof(n); ++i)
-	{
-		n |= (uint16_t)buffer[i] << (8 * i);
-	}
-
-	return n;
-}
-
-uint16_t frombigend16(uint8_t* buffer)
-{
-	uint16_t n = 0;
-	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
-	{
-		n |= (uint16_t)buffer[i] << (8 * k);
 		--k;
 	}
 
--- endianness/endianness.h
+++ endianness/endianness.h
@@ -22,15 +22,62 @@ inline static bool chkend()
 	return u.c[0] == 1;
 }
 
-void tobigend64(uint8_t* buffer, uint64_t n);
-void tobigend32(uint8_t* buffer, uint32_t n);
+inline static void tobigend64(uint8_t* buffer, uint64_t n)
+{
+	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
+	{
+		buffer[k] = (uint8_t)n;
+		n >>= 8;
+		--k;
+	}
+}
+
+inline static void tobigend32(uint8_t* buffer, uint32_t n)
+{
+	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
+	{
+		buffer[k] = (uint8_t)n;
+		n >>= 8;
+		--k;
+	}
+}
+
+inline static void tolitend32(uint8_t* buffer, uint32_t n)
+{
+	for (unsigned int i = 0; i < sizeof(n); ++i)
+	{
+		buffer[i] = (uint8_t)n;
+		n >>= 8;
+	}
+}
+
+inline static uint16_t fromlitend16(uint8_t* buffer)
+{
+	uint16_t n = 0;
+	for (unsigned int i = 0; i < sizeof(n); ++i)
+	{
+		n |= (uint16_t)buffer[i] << (8 * i);
+	}
+
+	return n;
+}
+
+inline static uint16_t frombigend16(uint8_t* buffer)
+{
+	uint16_t n = 0;
+	for (unsigned int i = 0, k = sizeof(n) - 1; i < sizeof(n); ++i)
+	{
+		n |= (uint16_t)buffer[i] << (8 * k);
+		--k;
+	}
+
+	return n;
+}
+
 void tobigend16(uint8_t* buffer, uint16_t n);
-void tolitend32(uint8_t* buffer, uint32_t n);
 void tolitend16(uint8_t* buffer, uint16_t n);
 uint32_t fromlitend32(uint8_t* buffer);
 uint32_t frombigend32(uint8_t* buffer);
-uint16_t fromlitend16(uint8_t* buffer);
-uint16_t frombigend16(uint8_t* buffer);
 
 #ifdef __cplusplus
 }
